2fb842176f44c66d57bbbf5ed5aaecf91deb4655
Umang Devanshu <devumang096@gmail.com>
2023-01-16 19:29:05 +0530

    func

--- function.js
+++ function.js
@@ -1,12 +1,27 @@
-function msg(){
-    console.log("hello world");
-}
-msg()
-//function
-let a=msg();
-console.log(a);
+// function msg(){
+//     console.log("hello world");
+// }
+// msg()
+// //function
+// let a=msg();
+// console.log(a);
 
-function add(a=10,b=6){
-    console.log(a+b);
+// function add(a=10,b=6){
+//     console.log(a+b);
+// }
+// add();
+
+function square(num){
+    return num*num;
 }
-add();
+const square = function(num){
+    return num*num;
+}
+const square = (num) => num*num;
+
+function Greeting(props){
+    return <h1>{props.message}</h1>
+}
+
+// Array.prototype.map 
+
