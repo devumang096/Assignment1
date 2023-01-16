d80223fa9edfd7009ea6db9a2986c1d70b0a7777
Umang Devanshu <devumang096@gmail.com>
2023-01-16 19:55:56 +0530

    map

--- function.js
+++ function.js
@@ -24,4 +24,23 @@ function Greeting(props){
 }
 
 // Array.prototype.map 
+//array.map() returns the new array by transforming each and every element
+//in the original array by applying a function to it.
+const numbers=[1,2,3,4,5,6,7,8,9];
+numbers.reverse() //mutable method
 
+const squareNumbers=numbers.map((num)=>num*num)
+
+const tenMultileArray = numbers.map((num)=>num*10)
+
+const stringArray=["Product1","Prodcut2","Prodcut3","Product4"]
+const elementArray = stringArray.map((product)=>{
+    return `<h1>${product}</h1>`
+})
+function modifyArray(stringArray){
+    let modifiedArray =[]
+    for(let i=0;i<stringArray.length;i++){
+        modifiedArray.push(`<h1>${stringArray[i]}</h1>`)
+    }
+    return modifiedArray
+}
