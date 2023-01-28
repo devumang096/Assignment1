a9f8c1e9fd42e2962e70692d17a48ef294c09120
Umang Devanshu <devumang096@gmail.com>
2023-01-28 16:15:14 +0530

    new project

---
+++ ReactJS/forms/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Heading from './Components/Heading';
+import Section1 from './Components/Section1';
+import Section2 from './Components/Section2'
+
+function App() {
+  return (
+    <div className="App">
+      <Heading /> 
+      <Section1 />
+      <Section2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/forms/src/Components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+        <h1>Hello world</h1>
+        Heading</div>
+  )
+}
+
+
+export default Heading;
