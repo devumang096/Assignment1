1f9a2e45e25f3d909a97e1c0ff8177122bda9386
Umang Devanshu <devumang096@gmail.com>
2023-02-02 16:22:04 +0530

    adding counter2

--- ReactJS/project1/src/App.js
+++ ReactJS/project1/src/App.js
@@ -3,12 +3,18 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter,setCounter] = useState(0);
-
+  const [counter2,setCounter2] = useState(0);
 
   useEffect(() =>{
     // console.log("use effect has run");
     document.title= `You clicked ${counter} times`;
-  });
+  }, [counter2]);
+
+  function handleClick(){
+    setCounter(counter+1);
+    if(counter %2 == 0)
+        setCounter2(counter);
+  }
   return (
    <div className="App">
     <p>Your counter value is : {counter}</p>
