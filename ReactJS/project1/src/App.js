b6ec4d121f8f83a6255d3ee95c9b1ac7f6d757f4
Umang Devanshu <devumang096@gmail.com>
2023-02-02 16:09:40 +0530

    adding reacthooksfile

---
+++ ReactJS/project1/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [counter,setCounter] = useState(0);
+
+
+  useEffect(() =>{
+    // console.log("use effect has run");
+    document.title= `You clicked ${counter} times`;
+  });
+  return (
+   <div className="App">
+    <p>Your counter value is : {counter}</p>
+    <button 
+    onClick={() => {
+      setCounter(counter +1);
+    }}> 
+    Counter</button>
+   </div>
+  );
+}
+
+export default App;
